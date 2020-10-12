75fd556bb145ec8b56dbe2d8941f7232368d0f00
jyin12 <yinjiayi5@gmail.com>
2020-10-12 12:13:51 -0400

    Retrive Hi/Lo temperature

--- src/app.js
+++ src/app.js
@@ -40,8 +40,17 @@ function displayWeatherInfo(response){
     let dateElement = document.querySelector("#timeDay");
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
+    //High temp
+    let highTempElement = document.querySelector(".hiTemp");
+    highTempElement.innerHTML = Math.round(response.data.main.temp_max);
+
+    //Low temp
+    let lowTempElement = document.querySelector(".loTemp");
+    lowTempElement.innerHTML = Math.round(response.data.main.temp_min);
+
     //Icon: finish later
     let iconElement = document.querySelector("#icon");
+    //if(response.data.weather[].icon.equals(""))
 
 }
 
@@ -54,6 +63,14 @@ function displayCity(event) {
   if (searchInput.value) {
     axios.get(url).then(displayWeatherInfo);
   }
+}
+
+//TODO 
+function displayFahrenheit(event){
+    event.preventDefault();
+    let celciusElement = document.querySelector("#temperature");
+    let fahrenheitConversion = (celciusElement.innerHTML * 9) / 5 + 32;
+    celciusElement.innerHTML = Math.round(fahrenheitConversion);
 }
 
 function currentPosition(position) {
@@ -78,5 +95,7 @@ searchCity.addEventListener("click", displayCity);
 let cityName = "Philadelphia";
 let api = "65a6ca53c94284a78ade1834db48e6ab";
 let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${api}&units=imperial`;
-
 axios.get(apiURL).then(displayWeatherInfo);
+
+let fahrenheit = document.querySelector("#f");
+fahrenheit.addEventListener("click", displayFahrenheit);
